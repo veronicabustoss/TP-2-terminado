ff3a635f71f6b426d6e255a04e4683bd0f6026e1
veronicabustoss <40859184+veronicabustoss@users.noreply.github.com>
2018-10-03 09:39:14 -0300

    Update main.c

--- Tp 2 terminado/main.c
+++ Tp 2 terminado/main.c
@@ -14,17 +14,6 @@ int main()
 
     initEmployees( empleados, MAX );
 
-    ///Hardcodeo
-    /*addEmployee(empleados, MAX, 0, "Pedro", "Soarez", 12000, 23);
-    addEmployee(empleados, MAX, 1, "Monica", "Barriento", 20000, 25);
-    addEmployee(empleados, MAX, 2, "Mabel", "Bustos", 1400, 28);
-    addEmployee(empleados, MAX, 3, "Jose", "Martinez", 12000, 22);
-    addEmployee(empleados, MAX, 4, "Carmen", "Rodriguez", 12000, 83);
-    addEmployee(empleados, MAX, 5, "Oscar", "Rodriguez", 50000, 1);
-    addEmployee(empleados, MAX, 6, "Mirta", "Rodriguez", 50000, 10);
-    addEmployee(empleados, MAX, 7, "Facundo", "Rodriguez", 50000, 1);
-    addEmployee(empleados, MAX, 8, "Pancho", "Rodriguez", 12000, 15);*/
-
 
 
     char name[50];
@@ -40,6 +29,21 @@ int main()
 
         switch(menuDeOpciones())//creo la funcion menu, donde me figura las opciones
         {
+            case 0:
+                flag=1;
+                
+                addEmployee(empleados, MAX, 0, "Pedro", "Soarez", 12000, 23);
+                addEmployee(empleados, MAX, 1, "Monica", "Barriento", 20000, 25);
+                addEmployee(empleados, MAX, 2, "Mabel", "Bustos", 1400, 28);
+                addEmployee(empleados, MAX, 3, "Jose", "Martinez", 12000, 22);
+                addEmployee(empleados, MAX, 4, "Carmen", "Rodriguez", 12000, 83);
+                addEmployee(empleados, MAX, 5, "Oscar", "Rodriguez", 50000, 1);
+                addEmployee(empleados, MAX, 6, "Mirta", "Rodriguez", 50000, 10);
+                addEmployee(empleados, MAX, 7, "Facundo", "Rodriguez", 50000, 1);
+                addEmployee(empleados, MAX, 8, "Pancho", "Rodriguez", 12000, 15);
+
+                break;
+                
         case 1:
             flag=1;
             lugarLibre=buscarLugarLibre(empleados, MAX);
